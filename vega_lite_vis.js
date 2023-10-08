49fa90524db11d654091ae7e7e531bcf3672570f
DominikSlovak <127165568+DominikSlovak@users.noreply.github.com>
2023-10-08 13:42:59 +1100

    commit

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,9 @@
 var vg_1 = "Data_Visualisation_2.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
+var vg_2 = "Data_Visualisation_1.vg.json";
+},
+vegaEmbed("#stacked_bar_chart", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}
+).catch(console.error));
