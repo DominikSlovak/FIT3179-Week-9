ab5bdc489ce2d6d291abea6b84b2dc23dc57fe15
DominikSlovak <127165568+DominikSlovak@users.noreply.github.com>
2023-10-12 12:42:29 +1100

    commit

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,6 +3,8 @@ var vg_2 = "Data_Visualisation_2.vg.json";
 var vg_3 = "Data_Visualisation_3.vg.json";
 var vg_4 = "Data_Visualisation_4.vg.json";
 var vg_5 = "Data_Visualisation_5.vg.json";
+var vg_6 = "Data_Visualisation_6.vg.json";
+var vg_7 = "Data_Visualisation_7.vg.json";
 
 vegaEmbed("#stacked_area_graph", vg_1).then(function(result1) {
   // Access the Vega view instance for the first visualization as result1.view
@@ -14,6 +16,12 @@ vegaEmbed("#stacked_area_graph", vg_1).then(function(result1) {
         // Access the Vega view instance for the fourth visualization as result4.view
         vegaEmbed("#heat_map", vg_5).then(function(result5) {
           // Access the Vega view instance for the fourth visualization as result5.view
+          vegaEmbed("#scatterplot", vg_6).then(function(result6) {
+            // Access the Vega view instance for the fourth visualization as result5.view
+            vegaEmbed("#stacked_bar_chart2", vg_7).then(function(result7) {
+              // Access the Vega view instance for the fourth visualization as result5.view
+            }).catch(console.error);
+          }).catch(console.error);
         }).catch(console.error);
       }).catch(console.error);
     }).catch(console.error);
